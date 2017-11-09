f7e20c3fa9ee544733c819d31faa58e96906d36f
723 <rb.natsumi@gmail.com>
2017-11-09 09:08:38 +0900

    instanting the Editor Class

--- lib/vir/editor.rb
+++ lib/vir/editor.rb
@@ -3,45 +3,48 @@ require "curses"
 
 module Vir
   class Editor
-    class << self
-    def editor_start
+    def initialize
       Curses.init_screen
       Curses.raw
       set_escdelay(25)
 
+      Curses.setpos(0,0)
+      Curses.addstr("Press Ctrl + c")
+      Curses.refresh
+
+      @win = Curses::Window.new(Curses.lines - 2,Curses.cols,2,0)
+      editor_start  
+    end
+
+    def editor_start
+      @win.scrollok(true)
       begin
-        Curses.setpos(0,0)
-        Curses.addstr("Press Ctrl + c")
-        Curses.refresh
+      @win.keypad(true)
 
-        win = Curses::Window.new(Curses.lines - 2,Curses.cols,2,0)
-        win.scrollok(true)
-        win.keypad(true)
+      x = 0
+      y = 0
 
-        x = 0
-        y = 0
-
-        loop do
-          key = nil
-          win.setpos(y,x)
-          key = win.getch
-          if key == Curses::KEY_CTRL_C
-            break
-          end
-          
-          case key
-            when 127
-              win.setpos(y,x - 1)
-              win.delch
-              x -= 1
-            when 10
-              x = 0
-              y += 1
-            else
-              print_key_info(win,y,x,key)
-              x += 1
-          end
+      loop do
+        key = nil
+        @win.setpos(y,x)
+        key = @win.getch
+        if key == Curses::KEY_CTRL_C
+          break
         end
+          
+        case key
+          when 127
+            @win.setpos(y,x - 1)
+            @win.delch
+            x -= 1
+          when 10
+            x = 0
+            y += 1
+          else
+            print_key_info(@win,y,x,key)
+            x += 1
+        end
+      end
       ensure
         Curses.close_screen
       end
@@ -55,7 +58,6 @@ module Vir
     def print_key_info(win,y,x,key)
       win.setpos(y,x)
       win.addch(key)
-    end
     end
   end
 end
